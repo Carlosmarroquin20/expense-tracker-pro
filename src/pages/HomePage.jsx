c76fbb59c5f4753deeb8c8293834bcd74ce01a3f
Emanuel Marroquin <carlosema322@gmail.com>
2024-12-17 15:36:06 -0600

    Add PropTypes validation for HomePage component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,23 +1,34 @@
+import PropTypes from "prop-types";
 import Header from "../components/Header";
 import ExpenseForm from "../components/ExpenseForm";
 import ExpenseList from "../components/ExpenseList";
 import TotalExpenses from "../components/TotalExpenses";
 
-const HomePage = () => {
+const HomePage = ({ expenses, addExpense, deleteExpense, totalExpenses }) => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-100 to-gray-100 flex flex-col items-center">
+    <div className="min-h-screen w-full bg-gray-100 flex flex-col">
       <Header />
-      <div className="w-full max-w-4xl bg-white shadow-lg rounded-lg p-6 my-8">
-        <ExpenseForm />
-        <div className="mt-6">
-          <ExpenseList />
-        </div>
-        <div className="mt-6">
-          <TotalExpenses />
-        </div>
+      <div className="w-full max-w-4xl mx-auto p-6 space-y-6">
+        <ExpenseForm addExpense={addExpense} />
+        <ExpenseList expenses={expenses} deleteExpense={deleteExpense} />
+        <TotalExpenses total={totalExpenses} />
       </div>
     </div>
   );
+};
+
+// PropTypes para validar las props recibidas
+HomePage.propTypes = {
+  expenses: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      amount: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+  addExpense: PropTypes.func.isRequired,
+  deleteExpense: PropTypes.func.isRequired,
+  totalExpenses: PropTypes.number.isRequired,
 };
 
 export default HomePage;
